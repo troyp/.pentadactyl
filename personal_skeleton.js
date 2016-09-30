6b8eed1666d11ea71ea3ed87c18d154ee2a8583d
Troy Pracy <troyp7@gmail.com>
2016-10-01 08:14:47 +1000

    new file

---
+++ personal_skeleton.js
@@ -0,0 +1,12 @@
+var personal = {
+    myemail:     "",
+    myemail2:    "",
+    myname:      "",
+    myfirstname: "",
+    mylastname:  "",
+    myaddress:   "",
+    mystreet:    "",
+    mysuburb:    "",
+    myphone:     "",
+    mymasterpw:  "",
+}
