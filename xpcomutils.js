e0c9b4f65d86a0494cd822ad441bf5f460998e62
Troy Pracy <troyp7@gmail.com>
2017-02-09 14:12:03 +1100

    add Cu; fix indentation

--- xpcomutils.js
+++ xpcomutils.js
@@ -6,33 +6,34 @@ function err(s) {
 
 var Cc = Components.classes;
 var Ci = Components.interfaces;
+var Cu = Components.utils;
 
 function XpcService(url, ifc) {
     try {
-	var interface = Ci[ifc];
-	return Cc[url].getService(interface);
+        var interface = Ci[ifc];
+        return Cc[url].getService(interface);
     } catch(e) {
-	err("XpcService: Cannot get service " + url + ", " + ifc + "\n" + e);
-	return null;
+        err("XpcService: Cannot get service " + url + ", " + ifc + "\n" + e);
+        return null;
     }
 }
 
 function XpcInstance(url, ifc) {
     try {
-	var interface = Ci[ifc];
-	return Cc[url].createInstance(interface);
+        var interface = Ci[ifc];
+        return Cc[url].createInstance(interface);
     } catch(e) {
-	err("XpcInstance: Cannot create instance " + url + ", " + ifc + "\n" + e);
-	return null;
+        err("XpcInstance: Cannot create instance " + url + ", " + ifc + "\n" + e);
+        return null;
     }
 }
 
 function XpcQueryInterface(elt, ifc) {
     try {
-	var interface = Ci[ifc];
-	return elt.QueryInterface(interface);
+        var interface = Ci[ifc];
+        return elt.QueryInterface(interface);
     } catch(e) {
-	err("XpcQueryInterface: Cannot query interface " + ifc + " of " + elt + "\n" + e);
-	return null;
+        err("XpcQueryInterface: Cannot query interface " + ifc + " of " + elt + "\n" + e);
+	      return null;
     }
 }
