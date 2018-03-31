dd1ec3e2d83e4ac9a14e9c7cf2e4304b2e91b97b
Troy Pracy <troyp7@gmail.com>
2018-04-01 00:54:50 +1100

    Add file listing source code file exports, shell function findexport
    * `src/doc/exports-list.csv` lists symbols exported by each source code file.
    * Environment variable `PENTADACTYL_SOURCE_DATA_DIR` stores location of
      `exports-list.csv`.
    * Shell function `findexport` for searching exports-list.

--- misc/sourcecode_utils.sh
+++ misc/sourcecode_utils.sh
@@ -2,6 +2,7 @@
 
 export PENTADACTYL_SOURCE_DIR='/home/troy/Documents/source/extensions/firefox/dactyl';
 export filelist="modules/buffer.jsm modules/commands.jsm modules/finder.jsm content/bookmarks.js content/browser.js content/commandline.js content/dactyl.js content/editor.js content/events.js content/hints.js content/mappings.js content/marks.js content/modes.js content/mow.js content/quickmarks.js content/tabs.js";
+export PENTADACTYL_SOURCE_DATA_DIR='/home/troy/.pentadactyl/src/data';
 
 findmapping() {
     (
@@ -27,5 +28,18 @@ findcommand() {
 	for f in $filelist; do
             grep -Hn "\"$1" $f;
 	done
+    )
+}
+
+findexport() {
+    (
+        cat "$PENTADACTYL_SOURCE_DATA_DIR/exports-list.csv" |
+            while read -r x || [[ -n $x ]]; do
+                key=`cut -d, -f1 <<< "$x"`;
+                val=`cut -d, -f2 <<< "$x"`;
+                if [[ "$val" =~ "$1" ]]; then
+                    echo "$x";
+                fi;
+            done;
     )
 }
