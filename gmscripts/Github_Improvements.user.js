37a428b3826e5e8afb2afd0e432be191ffa7cbc9
Troy Pracy <troyp7@gmail.com>
2020-09-23 21:28:48 +0000

    Group github: remove <site>al binding, replace with GM script

---
+++ gmscripts/Github_Improvements.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Github Improvements
+// @namespace   https://github.com/troyp
+// @include     https://*github.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+var url = document.location.href;
+
+if (url.match(/https:\/\/github\.com\/[^/]+\/[^/]+/)) {
+    Array.from(document.links).forEach(l=>l.id=l.id.replace("user-content-", ""));
+}
