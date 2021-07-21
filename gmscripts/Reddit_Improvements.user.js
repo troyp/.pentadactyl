cda2f4989a6b95c7400dbe0d29bfe6eb5b77e4af
Troy Pracy <troyp7@gmail.com>
2021-07-21 05:32:22 +0000

    Add userscript Reddit_Improvements.user.js

---
+++ gmscripts/Reddit_Improvements.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Reddit Improvements
+// @namespace   https://github.com/troyp
+// @match       *://*.reddit.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+// CONVERT LINKS TO OLD REDDIT
+var links = [...document.links];
+links.forEach(l=>l.href=l.href.replace(/^https?:\/\/[^./]*\.reddit\.com\//, "https://old.reddit.com/"));
