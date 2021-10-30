8d34da92a0fe30ead7111ca1362c568e250482dc
Troy Pracy <troyp7@gmail.com>
2021-10-30 05:37:54 +0000

    Reddit_Improvements.user.js: remove promoted content

--- gmscripts/Reddit_Improvements.user.js
+++ gmscripts/Reddit_Improvements.user.js
@@ -9,3 +9,7 @@
 // CONVERT LINKS TO OLD REDDIT
 var links = [...document.links];
 links.forEach(l=>l.href=l.href.replace(/^https?:\/\/[^./]*\.reddit\.com\//, "https://old.reddit.com/"));
+
+// REMOVE PROMOTED CONTENT
+var promoted = [...document.querySelectorAll(".promoted")];
+promoted.forEach(e=>e.remove());
